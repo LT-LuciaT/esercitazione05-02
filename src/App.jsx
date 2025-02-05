e1402e4b616563e7ab3257d4a7f6f0ce830c6c30
LT-LuciaT <lucia.trombin98@gmail.com>
2025-02-05 15:40:40 +0100

    2

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import MyNav from "./components/myNav";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <MyNav />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/MyNav.jsx
@@ -0,0 +1,22 @@
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+const MyNav = () => {
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">Epic-books</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ms-auto">
+            <Nav.Link href="#home">About</Nav.Link>
+            <Nav.Link href="#link">Browse</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default MyNav;
